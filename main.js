05e891519922cceec6adcccf85fe7e8eafb74221
Danii <128658303+DaniIvanike@users.noreply.github.com>
2023-11-08 16:27:49 -0300

    v

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
-function tocaSom (seletorAudio) {
-    const elemento = document.querySelector(seletorAudio);
+'use strict';
 
-    if (elemento && elemento.localName === 'audio') {
-        elemento.play();
-    }
-    else {
-        //alert('Elemento não encontrado');
-        console.log('Elemento não encontrado ou seletor inválido');
-    }
-
+function playSound(e) {	
+	const dataKey = e.type === 'mousedown' || e.type === 'touchstart' ? (this.dataset ? this.dataset.key : 49) : e.keyCode;
+	
+	const audio = document.querySelector(`audio[data-key="${dataKey}"]`);
+	const key = document.querySelector(`.key[data-key="${dataKey}"]`);
+	
+	if (!audio) return;
+	
+	audio.currentTime = 0;
+	audio.play();
+	
+	key.classList.add('key--playing');
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+function removeClass(e) {
+	const dataKey = e.type === 'mouseup' || e.type === 'touchend' ? (this.dataset ? this.dataset.key : 49) : e.keyCode;
+	
+	const key = document.querySelector(`.key[data-key="${dataKey}"]`);
+	
+	if (!key) return;
+	
+	key.classList.remove('key--playing');
+}
 
-//para
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+window.addEventListener('keydown', playSound);
+window.addEventListener('keyup', removeClass);
 
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
 
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    }
-
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => {
+	['mousedown', 'touchstart'].forEach(function(e) {
+		key.addEventListener(e, playSound);
+	});
+	['mouseup', 'touchend'].forEach(function(e) {
+		key.addEventListener(e, removeClass);
+	});
+})
     tecla.onkeydown = function (evento) {
 
         if (evento.code === 'Space' || evento.code === 'Enter') {
@@ -36,4 +49,4 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tecla.classList.remove('ativa');
     }
 
-}
+
